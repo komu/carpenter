562f345458c73eedb491d188fc7128c1dace216c
Juha Komulainen <juha.komulainen@evident.fi>
2015-03-31 07:30:43 +0300

    Added convenience methods for creating SubClassMatchers.

--- src/main/java/fi/evident/carpenter/SubClassMatcher.java
+++ src/main/java/fi/evident/carpenter/SubClassMatcher.java
@@ -2,6 +2,9 @@ package fi.evident.carpenter;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 /**
  * Base class for matchers that work with a specific sub class of S.
  */
@@ -25,4 +28,67 @@ public abstract class SubClassMatcher<T,S extends T> extends Matcher<T> {
 
     @NotNull
     protected abstract Match<T> matchSafely(@NotNull S value);
+
+
+    @NotNull
+    public static <S, T extends S, V> Matcher<S> of(@NotNull Class<T> type,
+                                                    @NotNull Function<V, S> ctor,
+                                                    @NotNull Function<T, Match<V>> matcher) {
+        return new SubClassMatcher<S, T>(type) {
+
+            @NotNull
+            @Override
+            protected Match<S> matchSafely(@NotNull T value) {
+                return Match.from(ctor, matcher.apply(value));
+            }
+        };
+    }
+
+    @NotNull
+    public static <S, T extends S, V1, V2> Matcher<S> of(@NotNull Class<T> type,
+                                                         @NotNull BiFunction<V1, V2, S> ctor,
+                                                         @NotNull Function<T, Match<V1>> matcher1,
+                                                         @NotNull Function<T, Match<V2>> matcher2) {
+        return new SubClassMatcher<S, T>(type) {
+
+            @NotNull
+            @Override
+            protected Match<S> matchSafely(@NotNull T value) {
+                return Match.from(ctor, matcher1.apply(value), matcher2.apply(value));
+            }
+        };
+    }
+
+    @NotNull
+    public static <S, T extends S, V1, V2, V3> Matcher<S> of(@NotNull Class<T> type,
+                                                             @NotNull Match.TernaryFunction<V1, V2, V3, S> ctor,
+                                                             @NotNull Function<T, Match<V1>> matcher1,
+                                                             @NotNull Function<T, Match<V2>> matcher2,
+                                                             @NotNull Function<T, Match<V3>> matcher3) {
+        return new SubClassMatcher<S, T>(type) {
+
+            @NotNull
+            @Override
+            protected Match<S> matchSafely(@NotNull T value) {
+                return Match.from(ctor, matcher1.apply(value), matcher2.apply(value), matcher3.apply(value));
+            }
+        };
+    }
+
+    @NotNull
+    public static <S, T extends S, V1, V2, V3, V4> Matcher<S> of(@NotNull Class<T> type,
+                                                                 @NotNull Match.QuadFunction<V1, V2, V3, V4, S> ctor,
+                                                                 @NotNull Function<T, Match<V1>> matcher1,
+                                                                 @NotNull Function<T, Match<V2>> matcher2,
+                                                                 @NotNull Function<T, Match<V3>> matcher3,
+                                                                 @NotNull Function<T, Match<V4>> matcher4) {
+        return new SubClassMatcher<S, T>(type) {
+
+            @NotNull
+            @Override
+            protected Match<S> matchSafely(@NotNull T value) {
+                return Match.from(ctor, matcher1.apply(value), matcher2.apply(value), matcher3.apply(value), matcher4.apply(value));
+            }
+        };
+    }
 }
